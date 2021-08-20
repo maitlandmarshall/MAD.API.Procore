70e931b6d3e3f0323919139f6c9fe0b6d63ee2b8
maitlandmarshall <maitlandmarshallapps@gmail.com>
2021-08-20 12:05:06 +1000

    Add ListTradesRequest

---
+++ MAD.API.Procore/Endpoints/Trades/ListTradesRequest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using MAD.API.Procore.Endpoints.Trades.Models;
+using MAD.API.Procore;
+using MAD.API.Procore.Requests;
+namespace MAD.API.Procore.Endpoints.Trades {
+	public class ListTradesRequest : ProcoreRequest<IEnumerable<Trade>> {
+
+		public override string Resource { get => $"/companies/{this.CompanyId}/trades";}
+
+		/// <summary>
+		/// Unique identifier for the company.
+		/// </summary>
+		[RequestParameter("company_id")]	public  long? CompanyId { get ; set; }
+
+		/// <summary>
+		/// Return item(s) last updated within the specified ISO 8601 datetime range.
+		/// </summary>
+		[RequestParameter("filters[updated_at]")]	public  string UpdatedAt { get ; set; }
+
+		/// <summary>
+		/// Limit results to available trades
+		/// </summary>
+		[RequestParameter("filters[active]")]	public  bool Active { get ; set; }
+	}
+}
