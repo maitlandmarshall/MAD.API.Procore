170659b3e876f19ebecb2a6494c4b560a71802f0
maitlandmarshall <maitlandmarshallapps@gmail.com>
2021-10-27 11:29:08 +1000

    Companies endpoint

---
+++ MAD.API.Procore/Endpoints/Companies/Models/ArrayOfCompany.cs
@@ -0,0 +1,9 @@
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+namespace MAD.API.Procore.Endpoints.Companies.Models {
+	public class ArrayOfCompany : List<Company> {
+	}
+}
