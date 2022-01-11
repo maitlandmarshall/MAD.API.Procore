f09f15761833d9b68aa7a3d968d41f21887c5610
Anthony Postlethwaite <dev.antpos@gmail.com>
2022-01-11 14:23:08 +1000

    Added procore API rate limit exception and updated ReadResponse method to throw exception instead of waiting when a http error 429 is received.

---
+++ MAD.API.Procore/ProcoreRateLimitException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MAD.API.Procore
+{
+    public class ProcoreRateLimitException : Exception
+    {
+        public DateTimeOffset RefreshDate { get; set; }
+
+        public ProcoreRateLimitException(DateTimeOffset refreshDate) : base($"Procore API rate limit exceeded. API rate limit will reset at {refreshDate.DateTime.ToLocalTime():dd/MM/yyyy HH:mm:ss}")
+        {
+            this.RefreshDate = refreshDate;
+        }
+    }
+}
